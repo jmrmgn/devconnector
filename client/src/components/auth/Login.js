8ca4157e375d101f25b8566f974e68a1e4a1a270
Jomar <jomar.magno026@gmail.com>
2019-01-06 01:15:26 +0800

    client login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+   constructor() {
+      super();
+      this.state = {
+         email: '',
+         password: ''
+      }
+   }
+
+   onChange = e => {
+      this.setState({
+         [e.target.name]: e.target.value
+      });
+   }
+
+   onSubmit = e => {
+      e.preventDefault();
+      const {  email, password } = this.state;
+      const user = { email, password };
+
+      console.log(user);
+   }
+
    render() {
       return(
-         <div>
-            <h1>Login</h1>
+         <div className="login">
+            <div className="container">
+               <div className="row">
+                  <div className="col-md-8 m-auto">
+                     <h1 className="display-4 text-center">Log In</h1>
+                     <p className="lead text-center">Sign in to your DevConnector account</p>
+                     <form onSubmit={this.onSubmit.bind(this)}>
+                        <div className="form-group">
+                        <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"
+                           value={this.state.email}
+                           onChange={this.onChange.bind(this)}
+                        />
+                        </div>
+                        <div className="form-group">
+                        <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"
+                           value={this.state.password}
+                           onChange={this.onChange.bind(this)}
+                        />
+                        </div>
+                        <input type="submit" className="btn btn-info btn-block mt-4" />
+                     </form>
+                  </div>
+               </div>
+            </div>
          </div>
       );
    }
