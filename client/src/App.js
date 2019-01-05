aedcda2f759ac0151b7bf4dadc317e2cfb0ca046
Jomar <jomar.magno026@gmail.com>
2019-01-06 00:57:37 +0800

    client react-router-dom setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -7,14 +10,24 @@ import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 
+// Auth Component
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 class App extends Component {
    render() {
       return (
-         <div className="App">
-            <Navbar />
-            <Landing />
-            <Footer />
-         </div>
+         <Router>
+            <div className="App">
+               <Navbar />
+               <Route exact path="/" component={Landing} />
+               <div className="container">
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+               </div>
+               <Footer />
+            </div>
+         </Router>
       );
    }
 }
